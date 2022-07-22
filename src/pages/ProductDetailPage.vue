70a89c1986e46e9454095a145cff77ff58649b15
ycoodr <ycoodr@gmail.com>
2022-07-22 16:24:50 +0300

    404 page

--- src/pages/ProductDetailPage.vue
+++ src/pages/ProductDetailPage.vue
@@ -1,9 +1,28 @@
 <template>
-<h1>Product detail page</h1>
+<div v-if="productFound">
+<h1>{{ product.name }}</h1>
+<h3>{{ product.price }}</h3>
+<p>{{ product.description }}</p>
+<button>Add to cart</button>
+
+</div>
+<div v-if="!productFound">
+<h1>Oops.. we couldn't find that product</h1>
+</div>
 </template>
 
 <script>
+import products from './products';
+
 export default {
     name: 'ProductDetailPage',
+    computed: {
+        product(){
+            return products.find(prod => prod.id === this.$route.params.id);
+        },
+        productFound(){
+            return !!this.product;
+        }
+    }
 };
 </script>
