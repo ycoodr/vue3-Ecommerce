d4f2e625fce0b6178bf9b16232c849b4725f910e
ycoodr <ycoodr@gmail.com>
2022-07-22 18:16:03 +0300

    form

---
+++ src/pages/UserInfoPage.vue
@@ -0,0 +1,55 @@
+<template>
+    <h1>User info</h1>
+    <div v-if="shouldShowValidationErrors">
+    <div v-for="error in validationErrors" :key="error">
+    {{ error }}
+    </div></div>
+    <div>
+        <input placeholder="Name" v-model="name" />
+        <input placeholder="Age" type="number" v-model.number="age" />
+        <input placeholder="Address" v-model="address" />
+        <button @click="displayData">Save</button>
+    </div>
+</template>
+
+
+
+<script>
+        export default {
+           name: 'UserInfoPage',
+           data(){
+            return {
+                name: '',
+                age: 0,
+                address: '',
+                shouldShowValidationErrors: false,
+            };
+           },
+           methods: {
+            displayData(){
+                this.shouldShowValidationErrors = true;
+                alert(`name is ${this.name}, age is ${this.age},
+                address is ${this.address}`);
+            }
+           },
+           computed: {
+            validationErrors(){
+                let errors = [];
+
+                if(this.name.length < 2){
+                    errors.push("Please enter a name longer than 2 characters");
+                }
+
+                if(this.age < 16){
+                    errors.push("Age must be 16 or greater");
+                }
+
+                if(this.address.length <= 5){
+                    errors.push("Please enter an address longer than 4 characters");
+                }
+
+                return errors;
+            }
+           }
+        }
+</script>
--- src/router.js
+++ src/router.js
@@ -2,11 +2,13 @@ import { createWebHistory, createRouter } from 'vue-router';
 import ProductsListPage from './pages/ProductsListPage';
 import ProductDetailPage from './pages/ProductDetailPage';
 import ShoppingCartPage from './pages/ShoppingCartPage';
+import UserInfoPage from './pages/UserInfoPage';
 
 const routes = [
     { path: '/', component: ProductsListPage },
     { path: '/products/:id', component: ProductDetailPage },
     { path: '/cart', component: ShoppingCartPage },
+    { path: '/user-info', component: UserInfoPage},
 ];
 
 const router = createRouter({
