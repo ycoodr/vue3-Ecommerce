2794b4a946c204fa686caf0d3a65043d35954a94
ycoodr <ycoodr@gmail.com>
2022-07-28 14:33:03 +0300

    axios

--- src/pages/EmployeeOfTheMonthPage.vue
+++ src/pages/EmployeeOfTheMonthPage.vue
@@ -1,9 +1,12 @@
 <template>
 <h1>Employee of the month</h1>
+<div v-if="personData">
 <h3>{{ personData.name.first }} {{ personData.name.last }}</h3>
+</div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
     name: 'EmployeeOfTheMonthPage',
     data(){
@@ -12,9 +15,12 @@ export default {
         }
     },
     created(){
-        fetch('https://randomuser.me/api')
-        .then(response => response.json())
-        .then(data => this.personData = data.results[0]);
+        // fetch('https://randomuser.me/api')
+        // .then(response => response.json())
+        // .then(data => this.personData = data.results[0]);
+
+        axios.get('https://randomuser.me/api')
+        .then(response => this.personData = response.data.results[0]);
     }
 }
 </script>
